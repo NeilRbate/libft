d1e6ad54abae3a2142df36f8cbd0ffa9fac725a3
Julien Barbate <jbarbate@c2r10p1.42nice.fr>
2022-11-08 11:50:16 +0100

    ATOI fully tested

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,30 +6,41 @@
 /*   By: jbarbate <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/08 08:36:40 by jbarbate          #+#    #+#             */
-/*   Updated: 2022/11/08 09:32:18 by jbarbate         ###   ########.fr       */
+/*   Updated: 2022/11/08 11:49:12 by jbarbate         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
+
+static int	ft_isspace(const char *str)
+{
+	if (*str == ' ' || *str == '\t' || *str == '\n'
+		|| *str == '\v' || *str == '\f' || *str == '\r')
+		return (1);
+	return (0);
+}
+
 int	ft_atoi(const char *str)
 {
-	int	i;
-	int	stock;
-	int	sign;
+	long	i;
+	long	stock;
+	long	sign;
 
 	i = 0;
 	stock = 0;
 	sign = 1;
-
+	while (ft_isspace(&str[i]) == 1)
+		i++;
 	if (str[i] == '-' || str[i] == '+')
 	{
 		if (str[i] == '-')
 			sign = -1;
 		i++;
 	}
-	while (str[i] >= '0' && str[i] <= '9' && str[i])
+	while ((str[i] >= '0' && str[i] <= '9') && str[i])
 	{
 		stock *= 10;
-		stock += (str[i] - '0');
+		stock += (str[i] - 48);
 		i++;
 	}
 	return ((stock * sign));
