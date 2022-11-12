f25c5a01342ecbf8fa73bec1f46f9d018cf157bc
Rbate <98659534+NeilRbate@users.noreply.github.com>
2022-11-12 14:45:47 +0100

    Add last ft, finish lib, just split memory crash to do

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -6,19 +6,33 @@
 /*   By: jbarbate <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/12 12:16:57 by jbarbate          #+#    #+#             */
-/*   Updated: 2022/11/12 12:48:32 by jbarbate         ###   ########.fr       */
+/*   Updated: 2022/11/12 14:42:10 by jbarbate         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-t_list	*ft_lstmap(t_list *lst, void *(*f)(void *))
+t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
-	if (lst != 0 && f != 0)
+	t_list	*list_1;
+	t_list	*list_2;
+
+	list_2 = 0;
+	if (lst != 0 && f != 0 && del != 0)
 	{
-		whiel (lst)
+		while (lst)
 		{
-			
+			list_1 =  ft_lstnew((*f)(lst->content));
+			if (list_1 == 0)
+			{
+				ft_lstclear(&list_2, (*del));
+				return (0);
+			}
+			ft_lstadd_back(&list_2, list_1);
+		       	lst = lst->next;
 		}
 	}
+	else
+		return (0);
+	return (list_2);
 }
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: jbarbate <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/08 07:05:59 by jbarbate          #+#    #+#             */
-/*   Updated: 2022/11/12 12:15:47 by jbarbate         ###   ########.fr       */
+/*   Updated: 2022/11/12 14:36:23 by jbarbate         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -64,5 +64,6 @@ void	ft_lstclear(t_list **lst, void (*del)(void *));
 void	ft_lstiter(t_list *lst, void (*f)(void *));
 t_list	*ft_lstnew(void *content);
 t_list	*ft_lstlast(t_list *lst);
+t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *));
 
 #endif
